29257443354b8f08ff3d04e7cad6aff89c4c7a66
Roger Vacovsky <rvacovsky@gmail.com>
2022-03-13 23:42:37 -0400

    fixed html

--- script.js
+++ script.js
@@ -3,6 +3,8 @@
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+//window.prompt to start questions
+var promptPassword = window.alert("Hello! I'm going to ask you a series of questions to generate your password. Ready? Let's begin!");
 
 // Write password to the #password input
 function writePassword() {
@@ -15,3 +17,6 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+/* Start password generator */
+generatePassword();
