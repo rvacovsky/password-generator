cee7a71fa1c866fcaf6800ff0f51fc371cec8789
Roger Vacovsky <rvacovsky@gmail.com>
2022-03-19 19:39:53 -0400

    added if statements

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
 // Assignment code here
-
+function generatePassword() {
+  const alpha = "abcdefghijklmnopqrstuvwxyz".split("");
+  const alphaUp = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+  const number = "1234567890".split("");
+  const symbol = "!@#^&*()?".split("");
+  var passwordLength = window.prompt ("What's the length of your password?")
+  if (passwordLength >= 8 && passwordLength <=128) {
+    window.confirm ("would you like to use uppercase letters?")
+    window.confirm ("would you like to use lowercase letters?")
+    window.confirm ("would you like to use numbers?")
+    window.confirm ("would you like to use special characters?")
+  } else {
+    window.alert("try again")
+    generatePassword();
+  }
+  
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
-//window.prompt to start questions
-var promptPassword = window.alert("Hello! I'm going to ask you a series of questions to generate your password. Ready? Let's begin!");
 
 // Write password to the #password input
 function writePassword() {
@@ -18,5 +32,3 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-/* Start password generator */
-generatePassword();
